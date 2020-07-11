1a85ed6b5ae4cf237ef03db1473cdfb3a093e438
Ubuntu <ubuntu@ip-172-31-26-198.us-east-2.compute.internal>
2020-07-11 02:16:38 +0000

    Creating a miracle

---
+++ bhargav/seed.groovy
@@ -0,0 +1,8 @@
+folder('Bhargav') {
+    displayName('Bhargav')
+    description('Folder for Bhargav')
+}
+
+folder('bhargav/testing') {
+    description('Folder containing all QA jobs for Bhargav')
+}"
