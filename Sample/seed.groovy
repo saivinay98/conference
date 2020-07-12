216e877308b491554b880d533f5d8e14da89af63
root <root@ip-172-31-37-80.ec2.internal>
2020-07-12 20:45:16 +0000

    update

---
+++ Sample/seed.groovy
@@ -0,0 +1,38 @@
+folder(JOB_FOLDER)
+
+
+
+pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
+
+
+
+    displayName('Ram')
+
+    description('Sample Job to create pipeline Job')
+
+
+
+    logRotator{
+
+        daysToKeep(90)
+
+    }
+
+
+
+    definition {
+
+        cps {
+
+            sandbox()
+
+            script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+
+        }
+
+    }
+
+
+
+}
+
