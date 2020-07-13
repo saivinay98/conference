31c0788e21f6a0df1efc25f82f4a3c2d556749f2
Ubuntu <ubuntu@ip-172-31-44-86.ec2.internal>
2020-07-13 16:53:47 +0000

    folder1

--- teja/seed.groovy
+++ teja/seed.groovy
@@ -1,16 +1,3 @@
-job('example-1') {
-    publishers {
-        buildPipelineTrigger('deploy-cluster-1, deploy-cluster-2')
-    }
-}
+folder("teja")
+folder("teja/folder1")
 
-job('example-2') {
-    publishers {
-        buildPipelineTrigger('deploy-cluster-1, deploy-cluster-2') {
-            parameters {
-                predefinedProp('GIT_COMMIT', '$GIT_COMMIT')
-                predefinedProp('ARTIFACT_BUILD_NUMBER', '$BUILD_NUMBER')
-            }
-        }
-    }
-}
